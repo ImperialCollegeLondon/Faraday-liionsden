1d493ae0ae227853dc50f436cdfc5e1ae4258b84
Tom Owen <tom.owen@animal-dynamics.com>
2020-11-10 12:07:01 +0000

    Basic PyQt GUI
    TODO: Select Columns to Import
    TODO: User Flows
    #TODO: Multiple parsers
    #TODO: Get DeviceC
    onfig actually working again..
    #TODO: Get automatic parsing going again
    TODO: Get data range storage & display working again
    TODO: Get plotting working again
    TODO: Get metadata JSON inheritance going
    TODO: REST API integration of new models, multiple objects per request
    TODO: Immutable History - HasImmutableHistory - ManyToMany: Common.HistoryLog (JSON)
    #TODO: DeviceCompositeStructure ? or is static structure enough? No, need something to define 2s2p pack layouts etc

---
+++ django-battdb/harvester-client/Harvester_gui.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import sys
+import typing
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+import time
+
+
+class MainWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        self.layout = QGridLayout(self)
+        self.logWidget = LogWidget(self)
+        self.uploadButton = QPushButton(self, text="Upload File")
+        self.dirWidget = DirectoryWidget(self)
+        self.layout.addWidget(self.logWidget, 3, 1)
+        self.layout.addWidget(self.uploadButton, 2, 1)
+        self.layout.addWidget(self.dirWidget, 1, 1)
+        self.uploadButton.clicked.connect(self.doUploadFile)
+
+    def doUploadFile(self):
+        self.logWidget.appendMessage("Uploading file %s" % self.dirWidget.get_selected_file())
+
+
+class LogWidget(QPlainTextEdit):
+    def __init__(self, parent=None, file="harvester.log"):
+        super(LogWidget,self).__init__(parent)
+        self.m_logFile = QFile(file)
+        self.m_logFile.open(QIODevice.Append | QIODevice.Text)
+        self.m_logStream = QTextStream(self.m_logFile)
+        self.setReadOnly(True)
+        self.appendMessage("=========================")
+        self.appendMessage("Harvester utility started")
+        self.appendMessage("=========================")
+
+    def appendMessage(self, msg):
+        logText = "[%s]: %s" % (time.strftime("%Y-%m-%d %H:%M:%S"), msg)
+        self.appendPlainText(logText)
+        self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+        self.m_logFile.write(bytes(logText+"\n", encoding='utf8'))
+        self.m_logFile.flush()
+
+
+class DirectoryWidget(QWidget):
+    def __init__(self, *args, **kwargs):
+        QWidget.__init__(self, *args, **kwargs)
+        hlay = QHBoxLayout(self)
+        self.treeview = QTreeView()
+        self.listview = QTableView()
+        hlay.addWidget(self.treeview)
+        hlay.addWidget(self.listview)
+
+        path = QDir.rootPath()
+
+        self.dirModel = QFileSystemModel()
+        self.dirModel.setRootPath("/home/towen")
+        self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
+
+        self.fileModel = HarvesterFileModel()
+        self.fileModel.setFilter(QDir.NoDotAndDotDot |  QDir.Files)
+
+        self.treeview.setModel(self.dirModel)
+        self.listview.setModel(self.fileModel)
+
+        self.treeview.setRootIndex(self.dirModel.index(path))
+        self.listview.setRootIndex(self.fileModel.index(path))
+
+        self.treeview.clicked.connect(self.on_clicked)
+
+    def get_selected_file(self):
+        return self.fileModel.filePath(self.listview.currentIndex())
+
+    def on_clicked(self, index):
+        path = self.dirModel.fileInfo(index).absoluteFilePath()
+        self.listview.setRootIndex(self.fileModel.setRootPath(path))
+
+
+class HarvesterFileModel(QFileSystemModel):
+    def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = ...) -> typing.Any:
+        if section == 2 and orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return "wibble"
+        return super().headerData(section, orientation, role)
+
+
+    def data(self, index: QtCore.QModelIndex, role: int = ...) -> typing.Any:
+        return super().data(index, role)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWidget()
+    w.show()
+    sys.exit(app.exec_())
