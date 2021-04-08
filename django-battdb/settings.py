b7df095d2510b4b9d44b8b1cdc60b4b13773a05b
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-04-08 18:11:19 +0100

    Adapt settings for GitHub Actions

--- django-battdb/settings.py
+++ django-battdb/settings.py
@@ -114,6 +114,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'postgres',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 
 #STATICFILES_FINDERS = {
 #      'django.contrib.staticfiles.finders.FileSystemFinder',
