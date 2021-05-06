670d0502c1a05357acd45460b0ea2d84df907761
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-05-06 14:13:04 +0100

    Add tests for common/utils.py

---
+++ common/tests/test_utils.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class TestHashFile(TestCase):
+    def test_hash_file(self):
+        import hashlib
+        from pathlib import Path
+
+        from ..utils import hash_file
+
+        f = open(Path(__file__), "rb")
+        expected = hashlib.md5(f.read()).hexdigest()
+        self.assertEqual(expected, hash_file(f))
