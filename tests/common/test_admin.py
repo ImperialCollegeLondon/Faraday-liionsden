8423055b10e0e6debcad264075562a1045c5f5ee
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-05-06 15:49:17 +0100

    Add tests for common/admin.py

---
+++ tests/common/test_admin.py
@@ -0,0 +1,102 @@
+from unittest import TestCase, skip
+
+from django.contrib.admin.sites import AdminSite
+
+
+class MockRequest:
+    pass
+
+
+request = MockRequest()
+
+
+@skip("It is unclear how to test this one")
+class TestBaseAdmin(TestCase):
+    pass
+
+
+class TestOrgAdmin(TestCase):
+    def setUp(self):
+        self.site = AdminSite()
+
+    def test_org_str(self):
+        from common.admin import OrgAdmin
+        from common.models import Org
+
+        ma = OrgAdmin(Org, self.site)
+        self.assertEqual(str(ma), "common.OrgAdmin")
+
+    def test_list_fields(self):
+        from common.admin import OrgAdmin
+        from common.models import Org
+
+        ma = OrgAdmin(Org, self.site)
+        self.assertEqual(
+            list(ma.get_list_display(request)),
+            [
+                "name",
+                "manager",
+                "website",
+                "is_research",
+                "is_publisher",
+                "is_mfg_cells",
+                "is_mfg_equip",
+            ],
+        )
+        self.assertEqual(
+            list(ma.get_list_filter(request)),
+            ["is_research", "is_publisher", "is_mfg_cells", "is_mfg_equip"],
+        )
+
+
+class TestPaperAdmin(TestCase):
+    def setUp(self):
+        self.site = AdminSite()
+
+    def test_paper_str(self):
+        from common.admin import PaperAdmin
+        from common.models import Paper
+
+        ma = PaperAdmin(Paper, self.site)
+        self.assertEqual(str(ma), "common.PaperAdmin")
+
+    def test_list_fields(self):
+        from common.admin import PaperAdmin
+        from common.models import Paper
+
+        ma = PaperAdmin(Paper, self.site)
+        self.assertEqual(
+            list(ma.get_list_display(request)), ["title", "DOI", "year", "has_pdf"]
+        )
+        self.assertEqual(
+            list(ma.get_list_filter(request)), ["year", "publisher", "authors"]
+        )
+
+
+class TestPersonAdmin(TestCase):
+    def setUp(self):
+        self.site = AdminSite()
+
+    def test_person_str(self):
+        from common.admin import PersonAdmin
+        from common.models import Person
+
+        ma = PersonAdmin(Person, self.site)
+        self.assertEqual(str(ma), "common.PersonAdmin")
+
+    def test_list_fields(self):
+        from common.admin import PersonAdmin
+        from common.models import Person
+
+        ma = PersonAdmin(Person, self.site)
+        self.assertEqual(
+            list(ma.get_list_display(request)), ["longName", "shortName", "user", "org"]
+        )
+        self.assertEqual(list(ma.get_list_filter(request)), ["org"])
+
+    def test_readonly_fields(self):
+        from common.admin import PersonAdmin
+        from common.models import Person
+
+        ma = PersonAdmin(Person, self.site)
+        self.assertEqual(list(ma.get_readonly_fields(request)), ["user_firstname"])
