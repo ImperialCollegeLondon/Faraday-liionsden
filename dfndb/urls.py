ef90cd322c30e0eaeb19b595acc0f607bfb566a7
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-05-07 14:14:14 +0100

    Disable API for Parameters

--- dfndb/urls.py
+++ dfndb/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("parameters/", views.ParametersAPIView.as_view(), name="parameters"),
+    # path("parameters/", views.ParametersAPIView.as_view(), name="parameters"),
 ]
