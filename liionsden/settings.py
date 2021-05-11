9516def471b9a5f5f70036ecdf24f6a214229e46
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-05-11 16:01:25 +0100

    Add static files to url patterns

--- liionsden/settings.py
+++ liionsden/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 INTERNAL_IPS = [
