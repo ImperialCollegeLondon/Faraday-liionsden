765b43ebb33f380bdd51a3d61f723ff284159a1e
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-06-30 14:31:16 +0100

    Update production.py

--- liionsden/settings/production.py
+++ liionsden/settings/production.py
@@ -3,7 +3,7 @@ import os
 from .settings import *  # noqa: F401, F403
 
 DEBUG = False
-ALLOWED_HOSTS = ["liionsden.rcs.ic.ac.uk", "liionsden.azurewebsites.net"]
+ALLOWED_HOSTS = ["liionsden.rcs.ic.ac.uk"]
 SECRET_KEY = os.environ["SECRET_KEY"]
 EMAIL_HOST = "smarthost.cc.ic.ac.uk"
 SERVER_EMAIL = "noreply@imperial.ac.uk"
@@ -13,3 +13,8 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_HSTS_SECONDS = 15552000
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+
+if os.environ.get("AZURE_WORKFLOW"):
+    ALLOWED_HOSTS = ["liionsden.azurewebsites.net"]
+    DATABASES["default"]["HOST"] = "liionsden-db.postgres.database.azure.com"
