d21e31e28e0611e81d03981cd37d2929f333261f
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-07-01 14:53:39 +0100

    Make env variables to show up in the shh session

--- docker/init.sh
+++ docker/init.sh
@@ -6,3 +6,6 @@ service ssh start
 
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
+
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile)
