536db194921d2d8b24e0a1d7960adfd57f24ad07
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-07-01 15:25:47 +0100

    Add origin of previous hack

--- docker/init.sh
+++ docker/init.sh
@@ -5,6 +5,7 @@ echo "Starting SSH ..."
 service ssh start
 
 # Get environment variables to show up in SSH session
+# hack taken form https://stackoverflow.com/a/65988789/3778792
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
 python manage.py migrate
