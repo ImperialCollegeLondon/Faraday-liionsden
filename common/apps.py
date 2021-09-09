232757973f98568000b6fe0ea2b5c13aaed13552
Daniel Davies <dan@dyn901-58.eduroam.ic.ac.uk>
2021-09-09 11:33:23 +0100

    Add preset group creation using post_migrate signal

--- common/apps.py
+++ common/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class CommonConfig(AppConfig):
     name = "common"
+
+    # Populate database with predefined roles
+    def ready(self):
+        from .signals import populate_groups
+        post_migrate.connect(populate_groups, sender=self)
