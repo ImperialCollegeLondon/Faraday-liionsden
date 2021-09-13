c9e8fb83845788b5b6e7b3714cdfe3a126af14f7
Daniel Davies <dan@dyn901-115.eduroam.ic.ac.uk>
2021-09-13 10:43:54 +0100

    actually remove redundant user group

--- management/signals.py
+++ management/signals.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Permission
 
 def populate_groups(sender, **kwargs):
     # Different user roles are defined as groups
-    user_roles = ['Administrator', 'User manager', 'Maintainer', 
+    user_roles = ['User manager', 'Maintainer', 
                 'Contributor', 'Read only']
     for name in user_roles:
         Group.objects.get_or_create(name=name)
