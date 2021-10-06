f82723c161d83fe9c7d322b8a661d8af8b536484
Dan Davies <d.w.davies@imperial.ac.uk>
2021-10-06 15:53:40 +0100

    set_permissions_modifiable for other battDB models

--- battDB/signals/handlers.py
+++ battDB/signals/handlers.py
@@ -6,19 +6,16 @@ from django.contrib.auth.models import Group
 import battDB.models as bdb
 
 
+# Models in the decorators below follow "standard" permission setup.
 @receiver(post_save, sender=bdb.DeviceSpecification)
-def set_standard_permissions(sender, instance, **kwargs):
-    """Set object-level permissions according to a standard setup.
-    Contributing user can modify the object if status is "private" but not
-    if "public".
+@receiver(post_save, sender=bdb.Batch)
+@receiver(post_save, sender=bdb.Equipment)
+def set_permissions_standard(sender, instance, **kwargs):
+    """Set object-level permissions according to a standard setup: The contributing
+    user can modify the object if status is "private" but not if "public".
     """
-
     # Get permissions for model
-    delete, change, view = (
-        f"delete_{sender._meta.model_name}",
-        f"change_{sender._meta.model_name}",
-        f"view_{sender._meta.model_name}",
-    )
+    delete, change, view = _get_perm_codenames(sender)
 
     # Maintainers get all perms
     for perm in [delete, change, view]:
@@ -30,5 +27,47 @@ def set_standard_permissions(sender, instance, **kwargs):
             assign_perm(perm, instance.user_owner, instance)
 
     elif instance.status.lower() == "public":
-        for group in "Read only", "Contributor":
+        for group in ["Read only", "Contributor"]:
             assign_perm(view, Group.objects.get(name=group), instance)
+
+
+@receiver(post_save, sender=bdb.Device)
+@receiver(post_save, sender=bdb.DeviceConfig)
+@receiver(post_save, sender=bdb.DeviceConfigNode)
+@receiver(post_save, sender=bdb.Experiment)
+@receiver(post_save, sender=bdb.ExperimentDataFile)
+@receiver(post_save, sender=bdb.UploadedFile)
+@receiver(post_save, sender=bdb.ExperimentDevice)
+@receiver(post_save, sender=bdb.DataColumn)
+@receiver(post_save, sender=bdb.DataRange)
+@receiver(post_save, sender=bdb.SignalType)
+def set_permissions_modifiable(sender, instance, **kwargs):
+    """Set object-level permissions according to a less strict setup: The contributing
+    user can modify the object if status is "private" or "public".
+    """
+    # Get permissions for model
+    delete, change, view = _get_perm_codenames(sender)
+
+    # Maintainers get all perms
+    for perm in [delete, change, view]:
+        assign_perm(perm, Group.objects.get(name="Maintainer"), instance)
+
+    # Contributing user
+    for perm in [change, view]:
+        assign_perm(perm, instance.user_owner, instance)
+
+    # Other users perms based on obj status
+    if instance.status.lower() == "public":
+        for group in ["Read only", "Contributor"]:
+            assign_perm(view, Group.objects.get(name=group), instance)
+
+
+def _get_perm_codenames(model):
+    """Helper function to get delete, change and view permission codenames for a
+    given model.
+    """
+    return (
+        f"delete_{model._meta.model_name}",
+        f"change_{model._meta.model_name}",
+        f"view_{model._meta.model_name}",
+    )
