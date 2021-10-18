aa8bad79602ca0e41b19fb295a972c2185b9e520
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-10-18 18:55:21 +0100

    Use django-cleanup to delete files from disk. Close #5

--- liionsden/settings/settings.py
+++ liionsden/settings/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "guardian",
+    "django_cleanup.apps.CleanupConfig",  # <-- This must be last
 ]
 
 AUTHENTICATION_BACKENDS = (
