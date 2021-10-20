8781b043e76a54070e277101bafa792c7ae8e38c
Dan Davies <d.w.davies@imperial.ac.uk>
2021-10-20 11:30:22 +0100

    Add further object perm tests

--- tests/battDB/test_object_permissions.py
+++ tests/battDB/test_object_permissions.py
@@ -7,16 +7,24 @@ from model_bakery import baker
 User = get_user_model()
 
 
-class TestCreatePrivate(TestCase):
-    def setUp(self):
-        self.model = baker.make_recipe("tests.battDB.batch", status="private")
-        self.model_owner = self.model.user_owner
+class BaseObjectTest(TestCase):
+    @classmethod
+    def setUpClass(self):
+        super(BaseObjectTest, self).setUpClass()
         self.read_only = baker.make_recipe("tests.management.user")
         Group.objects.get(name="Read only").user_set.add(self.read_only)
         self.contributor = baker.make_recipe("tests.management.user")
         Group.objects.get(name="Contributor").user_set.add(self.contributor)
         self.maintainer = baker.make_recipe("tests.management.user")
         Group.objects.get(name="Maintainer").user_set.add(self.maintainer)
+
+
+class TestCreatePrivate(BaseObjectTest):
+    @classmethod
+    def setUpClass(self):
+        super(TestCreatePrivate, self).setUpClass()
+        self.model = baker.make_recipe("tests.battDB.batch", status="private")
+        self.model_owner = self.model.user_owner
 
     def test_user_perms(self):
         self.assertTrue(
@@ -28,27 +36,25 @@ class TestCreatePrivate(TestCase):
         self.assertFalse(self.contributor.has_perm("battDB.change_batch", self.model))
 
     def test_group_perms(self):
-        self.assertNotIn(
-            self.model, get_objects_for_user(self.contributor, "battDB.view_batch")
-        )
         for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
             with self.subTest(perm):
                 self.assertNotIn(self.model, get_objects_for_user(self.read_only, perm))
         for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
             with self.subTest(perm):
+                self.assertNotIn(
+                    self.model, get_objects_for_user(self.contributor, perm)
+                )
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
                 self.assertIn(self.model, get_objects_for_user(self.maintainer, perm))
 
 
-class TestCreatePublic(TestCase):
-    def setUp(self):
+class TestCreatePublic(BaseObjectTest):
+    @classmethod
+    def setUpClass(self):
+        super(TestCreatePublic, self).setUpClass()
         self.model = baker.make_recipe("tests.battDB.batch", status="public")
         self.model_owner = self.model.user_owner
-        self.read_only = baker.make_recipe("tests.management.user")
-        Group.objects.get(name="Read only").user_set.add(self.read_only)
-        self.contributor = baker.make_recipe("tests.management.user")
-        Group.objects.get(name="Contributor").user_set.add(self.contributor)
-        self.maintainer = baker.make_recipe("tests.management.user")
-        Group.objects.get(name="Maintainer").user_set.add(self.maintainer)
 
     def test_user_perms(self):
         self.assertFalse(self.model_owner.has_perm("battDB.change_batch", self.model))
@@ -73,6 +79,62 @@ class TestCreatePublic(TestCase):
                 self.assertIn(self.model, get_objects_for_user(self.maintainer, perm))
 
 
-# class TestPublicToPrivate(TestCase):
+class TestPublicToPrivate(BaseObjectTest):
+    @classmethod
+    def setUpClass(self):
+        super(TestPublicToPrivate, self).setUpClass()
+        self.model = baker.make_recipe("tests.battDB.batch", status="public")
+        self.model.status = "private"
+        self.model.save()
+        self.model_owner = self.model.user_owner
 
-# class TestDelete(TestCase):
+    def test_user_perms(self):
+        self.assertTrue(
+            self.model_owner.has_perms(
+                ["battDB.view_batch", "battDB.change_batch"], self.model
+            )
+        )
+        self.assertFalse(self.model_owner.has_perm("battDB.delete_batch", self.model))
+        self.assertFalse(self.contributor.has_perm("battDB.change_batch", self.model))
+
+    def test_group_perms(self):
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
+                self.assertNotIn(self.model, get_objects_for_user(self.read_only, perm))
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
+                self.assertNotIn(
+                    self.model, get_objects_for_user(self.contributor, perm)
+                )
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
+                self.assertIn(self.model, get_objects_for_user(self.maintainer, perm))
+
+
+class TestPublicToDeleted(BaseObjectTest):
+    @classmethod
+    def setUpClass(self):
+        super(TestPublicToDeleted, self).setUpClass()
+        self.model = baker.make_recipe("tests.battDB.batch", status="public")
+        self.model.status = "deleted"
+        self.model.save()
+        self.model_owner = self.model.user_owner
+
+    def test_user_perms(self):
+        self.assertFalse(self.model_owner.has_perm("battDB.view_batch", self.model))
+        self.assertFalse(self.model_owner.has_perm("battDB.change_batch", self.model))
+        self.assertFalse(self.model_owner.has_perm("battDB.delete_batch", self.model))
+        self.assertFalse(self.contributor.has_perm("battDB.change_batch", self.model))
+
+    def test_group_perms(self):
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
+                self.assertNotIn(self.model, get_objects_for_user(self.read_only, perm))
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
+                self.assertNotIn(
+                    self.model, get_objects_for_user(self.contributor, perm)
+                )
+        for perm in ["battDB.view_batch", "battDB.change_batch", "battDB.delete_batch"]:
+            with self.subTest(perm):
+                self.assertIn(self.model, get_objects_for_user(self.maintainer, perm))
