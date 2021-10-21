278a11d5678d746acff8657045274a473b2ffb8a
Dan Davies <d.w.davies@imperial.ac.uk>
2021-10-21 12:08:59 +0100

    Update model tests

--- tests/common/baker_recipes.py
+++ tests/common/baker_recipes.py
@@ -1,6 +1,8 @@
 from model_bakery.recipe import Recipe, foreign_key
 
-from common.models import Org
+from common.models import Org, Paper
 from tests.management.baker_recipes import user
 
 org = Recipe(Org, manager=foreign_key(user))
+
+paper = Recipe(Paper)
