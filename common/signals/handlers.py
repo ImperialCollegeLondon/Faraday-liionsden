27dca78c8e20ee2a2c3678f86147ce1a8ce71ae1
Daniel Davies <d.w.davies@imperial.ac.uk>
2021-10-22 12:48:12 +0100

    Add basic perms framework for common

---
+++ common/signals/handlers.py
@@ -0,0 +1,64 @@
+from django.contrib.auth.models import Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from guardian.shortcuts import assign_perm, remove_perm
+
+import common.models as cm
+
+
+@receiver(post_save, sender=cm.Person)
+@receiver(post_save, sender=cm.Paper)
+@receiver(post_save, sender=cm.Org)
+def set_permissions(sender, instance, **kwargs):
+    """Set object-level permissions according to a standard setup: The contributing
+    user can modify the object if status is "private" but not if "public".
+    """
+    # Get permissions for model
+    delete, change, view = _get_perm_codenames(sender)
+
+    # Maintainers get all perms
+    for perm in [delete, change, view]:
+        assign_perm(perm, Group.objects.get(name="Maintainer"), instance)
+
+    # If Org or Person, these are visible to everyone
+    if type(instance) in [cm.Org, cm.Person]:
+        for group in ["Read only", "Contributor"]:
+            assign_perm(view, Group.objects.get(name=group), instance)
+
+    elif type(instance) == cm.Paper:
+        # Permissions based on obj status
+        if instance.status.lower() == "private":
+            for perm in [change, view]:
+                assign_perm(perm, instance.user_owner, instance)
+            for group in ["Read only", "Contributor"]:
+                remove_perm(view, Group.objects.get(name=group), instance)
+
+        elif instance.status.lower() == "public":
+            remove_perm(change, instance.user_owner, instance)
+            for group in ["Read only", "Contributor"]:
+                assign_perm(view, Group.objects.get(name=group), instance)
+
+        elif instance.status.lower() == "deleted":
+            for group in ["Read only", "Contributor"]:
+                remove_perm(view, Group.objects.get(name=group), instance)
+            for perm in [change, view]:
+                remove_perm(perm, instance.user_owner, instance)
+
+        else:
+            raise ValueError("Object status must be private, public or deleted.")
+
+    else:
+        raise NotImplementedError(
+            f"Permissions framework not set up for {type(instance)}; cannot save object."
+        )
+
+
+def _get_perm_codenames(model):
+    """Helper function to get delete, change and view permission codenames for a
+    given model.
+    """
+    return (
+        f"delete_{model._meta.model_name}",
+        f"change_{model._meta.model_name}",
+        f"view_{model._meta.model_name}",
+    )
