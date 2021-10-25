d2ffb077987a1a5f5a628d93b87638535d295c96
Dan Davies <d.w.davies@imperial.ac.uk>
2021-10-25 09:01:38 +0100

    Split dfndb permissions handler

--- dfndb/signals/handlers.py
+++ dfndb/signals/handlers.py
@@ -6,8 +6,6 @@ from guardian.shortcuts import assign_perm, remove_perm
 import dfndb.models as dfn
 
 
-@receiver(post_save, sender=dfn.Compound)
-@receiver(post_save, sender=dfn.QuantityUnit)
 @receiver(post_save, sender=dfn.Parameter)
 @receiver(post_save, sender=dfn.Data)
 @receiver(post_save, sender=dfn.Method)
@@ -25,32 +23,44 @@ def set_permissions(sender, instance, **kwargs):
     for perm in [delete, change, view]:
         assign_perm(perm, Group.objects.get(name="Maintainer"), instance)
 
-    # If Compound or QuantityUnit, these are visible to everyone
-    if type(instance) in [dfn.Compound, dfn.QuantityUnit]:
+    # Permissions based on obj status
+    if instance.status.lower() == "private":
+        for perm in [change, view]:
+            assign_perm(perm, instance.user_owner, instance)
+        for group in ["Read only", "Contributor"]:
+            remove_perm(view, Group.objects.get(name=group), instance)
+
+    elif instance.status.lower() == "public":
+        remove_perm(change, instance.user_owner, instance)
         for group in ["Read only", "Contributor"]:
             assign_perm(view, Group.objects.get(name=group), instance)
 
+    elif instance.status.lower() == "deleted":
+        for group in ["Read only", "Contributor"]:
+            remove_perm(view, Group.objects.get(name=group), instance)
+        for perm in [change, view]:
+            remove_perm(perm, instance.user_owner, instance)
+
     else:
-        # Permissions based on obj status
-        if instance.status.lower() == "private":
-            for perm in [change, view]:
-                assign_perm(perm, instance.user_owner, instance)
-            for group in ["Read only", "Contributor"]:
-                remove_perm(view, Group.objects.get(name=group), instance)
+        raise ValueError("Object status must be private, public or deleted.")
 
-        elif instance.status.lower() == "public":
-            remove_perm(change, instance.user_owner, instance)
-            for group in ["Read only", "Contributor"]:
-                assign_perm(view, Group.objects.get(name=group), instance)
 
-        elif instance.status.lower() == "deleted":
-            for group in ["Read only", "Contributor"]:
-                remove_perm(view, Group.objects.get(name=group), instance)
-            for perm in [change, view]:
-                remove_perm(perm, instance.user_owner, instance)
+@receiver(post_save, sender=dfn.Compound)
+@receiver(post_save, sender=dfn.QuantityUnit)
+def set_visible_to_all(sender, instance, **kwargs):
+    """Set object-level permissions according to a setup suitable for very generic
+    objects with no status or owner: The saved model is visible to all authenticated
+    users and editable by maintainers.
+    """
+    # Get permissions for model
+    delete, change, view = _get_perm_codenames(sender)
 
-        else:
-            raise ValueError("Object status must be private, public or deleted.")
+    # Maintainers get all perms
+    for perm in [delete, change, view]:
+        assign_perm(perm, Group.objects.get(name="Maintainer"), instance)
+
+    for group in ["Read only", "Contributor"]:
+        assign_perm(view, Group.objects.get(name=group), instance)
 
 
 def _get_perm_codenames(model):
