8bcd4b7ed1c0d3fb3bda2b42164ebbf83002fb58
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-01-04 12:10:59 +0000

    Filter on contains and remove valid ticks

--- battDB/filters.py
+++ battDB/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db import models
 
 from .models import Experiment
 
@@ -6,4 +7,13 @@ from .models import Experiment
 class ExperimentFilter(django_filters.FilterSet):
     class Meta:
         model = Experiment
-        fields = ["id", "name", "date"]
+        fields = ["id", "name", "date", "status"]
+
+        filter_overrides = {
+            models.CharField: {
+                "filter_class": django_filters.CharFilter,
+                "extra": lambda f: {
+                    "lookup_expr": "icontains",
+                },
+            }
+        }
