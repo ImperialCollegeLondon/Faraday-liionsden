b19eeb59d2a70b8f99c29fc4bed206373b82b0a3
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-01-05 11:52:39 +0000

    Add date range fields

--- battDB/filters.py
+++ battDB/filters.py
@@ -1,18 +1,24 @@
-import django_filters
 from django.db import models
+from django_filters import CharFilter, DateFilter, FilterSet
+from django_filters.filters import DateFromToRangeFilter
+from django_filters.widgets import RangeWidget
 
 from .models import Experiment
 
 
-class ExperimentFilter(django_filters.FilterSet):
+class ExperimentFilter(FilterSet):
+    date = DateFromToRangeFilter(
+        widget=RangeWidget(attrs={"type": "date"}), label="Date range"
+    )
+
     class Meta:
         model = Experiment
-        fields = ["id", "name", "date", "status"]
+        fields = ["id", "name", "status", "user_owner"]
 
         # Allow filtering on partial matches for charfield
         filter_overrides = {
             models.CharField: {
-                "filter_class": django_filters.CharFilter,
+                "filter_class": CharFilter,
                 "extra": lambda f: {
                     "lookup_expr": "icontains",
                 },
