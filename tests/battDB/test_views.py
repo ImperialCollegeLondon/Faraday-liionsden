524dc5750a27fa2f087989cc4c50cdf8b2c39ce5
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-01-19 15:13:46 +0000

    Add failing devicespec update test

--- tests/battDB/test_views.py
+++ tests/battDB/test_views.py
@@ -51,3 +51,56 @@ class EquipmentUpdateTest(TestCase):
 
         self.model.refresh_from_db()
         self.assertEqual(self.model.name, "A different cycler")
+
+
+class DeviceSpecificationUpdateTest(TestCase):
+    def setUp(self):
+        self.user = baker.make_recipe(
+            "tests.management.user",
+            username="super",
+            is_superuser=True,
+        )
+        self.user.set_password("superpass")
+        self.user.save()
+
+        self.model_abstract = baker.make_recipe(
+            "tests.battDB.device_specification", abstract=True
+        )
+        self.model = baker.make_recipe(
+            "tests.battDB.device_specification",
+            name="Test spec",
+            user_owner=self.user,
+            device_type=self.model_abstract,
+        )
+
+    def test_owner(self):
+        self.assertEqual(self.model.user_owner.username, self.user.username)
+
+    def test_user(self):
+        self.assertEqual(self.user.username, "super")
+        self.assertTrue(self.user.is_superuser)
+
+    def test_update_device_specification(self):
+        # Check login OK
+        login_response = self.client.post(
+            "/accounts/login/", {"username": "super", "password": "superpass"}
+        )
+        self.assertEqual(login_response.status_code, 302)
+        self.assertEqual(login_response.url, "/")
+
+        # Check initial model name
+        self.assertEqual(self.model.name, "Test spec")
+
+        # Change name and check again
+        response = self.client.post(
+            reverse("battDB:Update Device", kwargs={"pk": self.model.id}),
+            {
+                "name": "A different device spec",
+                "device_type": self.model_abstract.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/battDB/devices/")
+
+        self.model.refresh_from_db()
+        self.assertEqual(self.model.name, "A different device spec")
