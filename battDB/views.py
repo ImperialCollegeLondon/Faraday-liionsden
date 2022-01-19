19a36d33e637da76cbb14f9eebd5006b6969c856
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-01-19 10:54:10 +0000

    correct troubleshooting comment

--- battDB/views.py
+++ battDB/views.py
@@ -251,7 +251,7 @@ class UpdateDataInlineView(UpdateView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         context = self.get_context_data()
-        # form.instance.user_owner is None !!
+        # form.instance.user_owner is correct !!
         # self.object.user_owner is correct !!
         parameters = context[self.inline_key]
         if form.is_valid():
