f29260d2e3a9ebece9dbe6d380381c5f076d8ac5
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-02-09 12:08:09 +0000

    Make media available always, not just in DBEUG mode

--- liionsden/urls.py
+++ liionsden/urls.py
@@ -28,5 +28,4 @@ urlpatterns = [
     path("dfndb/", include("dfndb.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
