afc661a8cc8ac569a2f10c04a67af7f12cfaa4b9
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-02-10 16:00:54 +0000

    Refactor Maccor engine

--- parsing_engines/maccor_engine.py
+++ parsing_engines/maccor_engine.py
@@ -1,11 +1,24 @@
-import logging
-import os
-from typing import Dict, Generator, Iterable, List, Tuple
+from typing import (
+    Any,
+    Dict,
+    Generator,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Text,
+    Tuple,
+    Union,
+)
 
+import pandas as pd
 import xlrd
+from numpy.typing import NDArray
+from pandas.core.dtypes.common import is_numeric_dtype
 from xlrd.sheet import Cell
 
 from .battery_exceptions import EmptyFileError
+from .mappings import COLUMN_NAME_MAPPING
 from .parsing_engines_base import ParsingEngineBase
 
 
@@ -20,167 +33,204 @@ class MaccorXLSParser(ParsingEngineBase):
     valid: List[Tuple[str, str]] = [
         ("application/vnd.ms-excel", ".xls"),
         ("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", ".xlsx"),
-        ("text/plain", ".txt"),
     ]
+    MANDATORY_COLUMNS = {"Cyc#", "Step"}
 
     def __init__(self, file_path: str) -> None:
         """Initialises the XLS parser for Maccor"""
         super().__init__(file_path)
-        self.workbook = xlrd.open_workbook(file_path, on_demand=True)
-        # Rec# columns in a typical Maccor file, set in get_metadata, used in get_data
-        self.rec_col = -1
+        workbook = xlrd.open_workbook(file_path, on_demand=True)
+        self.sheet = workbook.sheet_by_index(0)
+        self.datemode = workbook.datemode
 
-    @staticmethod
-    def clean_value(value: str) -> str:
-        """Trims values"""
-        return value.replace("''", "'").strip().rstrip("\0").strip()
+        self.skip_rows = self._get_header_size()
+        self.data = self._load_data()
 
-    def _get_metadata_value(self, key: str, value: Cell) -> str:
-        """A wrapper for metadata value parsing. Handles special cases."""
-        if "Date" in key:
-            return xlrd.xldate.xldate_as_datetime(value.value, self.workbook.datemode)
-        elif "Procedure" in key:
-            return self.clean_value(value.value) + "\t" + self.clean_value(value.value)
+        # Some preprocessing that can be done straight away
+        self._drop_unnamed_columns()
+        self._standardise_columns()
+        self._create_rec_no()
+
+    def _get_metadata_value(
+        self, idx: int, row: Sequence[Cell]
+    ) -> Tuple[str, str, int]:
+        """A wrapper for metadata value parsing, handling special cases.
+
+        Args:
+            idx (int): Index of the cell with the next key to check
+            row (Sequence[Cell]): Row of cells
+
+        Raises:
+            ValueError: If the index of the next cell is not bigger than the current
+                one.
+
+        Returns:
+            Tuple[str, str, int]: A tuple with the processed key, value and the next
+            index to check.
+        """
+        key = row[idx]
+        if not key:
+            key = ""
+        elif isinstance(key.value, Text):
+            key = key.value.replace("''", "'").strip().rstrip(":")
         else:
-            return value.value
+            key = key.value
 
-    def _identify_columns(self, header_row: int) -> (Dict, Dict):
-        """Parses the file to determine column headers as well as which columns have data"""
-        sheet = self.workbook.sheet_by_index(0)
-        headers = [x.value for x in sheet.row_slice(header_row)]
-        column_has_data = [False for _ in range(sheet.ncols)]
+        if "Date" in key:
+            value = xlrd.xldate.xldate_as_datetime(row[idx + 1].value, self.datemode)
+            new_idx = idx + 2
+        elif "Procedure" in key:
+            value = (
+                clean_value(row[idx + 1].value) + ", " + clean_value(row[idx + 2].value)
+            )
+            new_idx = idx + 3
+        else:
+            value = row[idx + 1].value if idx + 1 < len(row) else ""
+            new_idx = idx + 2
 
-        column_is_numeric, numeric_columns = [], []
-        val_row = sheet.row_slice(header_row + 1)
-        for i in range(sheet.ncols):
-            column_is_numeric.insert(i, isinstance(val_row[i].value, float))
-            if column_is_numeric[i]:
-                numeric_columns.append(i)
-            else:
-                column_has_data.insert(i, True)
+        if new_idx <= idx:
+            raise ValueError(
+                "Non-increasing index number when processing metadata cells."
+            )
+        return key, value, new_idx
 
-        first_rec = 1
-        try:
-            self.rec_col = headers.index("Rec#")
-            first_rec = sheet.cell_value(header_row + 1, self.rec_col)
-        except ValueError:
-            pass
+    def _get_header_size(self) -> int:
+        """Reads the file and determines the size of the header.
 
-        last_rec, total_rows = self._check_columns_for_data(
-            column_has_data, headers, numeric_columns, header_row + 1
+        Returns:
+            Header size as an int
+        """
+        for i, row in enumerate(self.sheet.get_rows()):
+            if not is_metadata_row(row, self.MANDATORY_COLUMNS):
+                return i
+        return 0
+
+    def _load_data(self) -> pd.DataFrame:
+        """Loads the data as a Pandas data frame.
+
+        Returns:
+            A pandas dataframe with all the data.
+        """
+        data = pd.read_excel(
+            self.file_path, sheet_name=None, header=self.skip_rows, index_col=None
         )
 
-        return (
-            {
-                "num_rows": total_rows,
-                "first_sample_no": first_rec,
-                "last_sample_no": last_rec,
-            },
-            {
-                headers[i]: {
-                    "has_data": column_has_data[i],
-                    "is_numeric": column_is_numeric[i],
-                }
-                for i in range(len(headers))
-            },
-        )
+        if isinstance(data, dict):
+            data = pd.concat(data.values(), ignore_index=True)
 
-    def _check_columns_for_data(
-        self, column_has_data, headers, numeric_columns, data_start
-    ):
-        """Scans the entire file for datapoints in each column"""
-        total_rows, last_rec = 0, 0
-        for sheet_id in range(self.workbook.nsheets):
-            logging.info("Loading sheet for metadata parsing %d", sheet_id)
-            sheet = self.workbook.sheet_by_index(sheet_id)
-            total_rows += sheet.nrows - 2
-            for col in numeric_columns[:]:
-                try:
-                    unique_vals = set(
-                        [float(x) for x in sheet.col_values(col, data_start)]
-                    )
-                except (ValueError, IndexError) as e:
-                    unique_vals = set()
+        return data
 
-                unique_vals.discard(0)
-                if len(unique_vals) > 0:
-                    column_has_data[col] = True
-                    numeric_columns.remove(col)
-                    logging.info("Found data in col %d", col)
+    def _create_rec_no(self) -> None:
+        """Adds Rec# to the dataset if it was not already present."""
+        if "Rec#" not in self.data.columns:
+            self.data["Rec#"] = range(len(self.data))
 
-            if sheet.nrows > 2:
-                try:
-                    last_rec = sheet.cell_value(2, headers.index("Rec#"))
-                except ValueError:
-                    last_rec = total_rows
+    def _drop_unnamed_columns(self) -> None:
+        """Drops columns of the internal parser dataframe that have no name."""
+        self.data = self.data.loc[:, ~self.data.columns.str.contains("^Unnamed")]
 
-            logging.info("Unloading sheet %d", sheet_id)
-            self.workbook.unload_sheet(sheet_id)
-        return last_rec, total_rows
+    def _standardise_columns(self) -> None:
+        """Standardise column names using a mapping of standard names."""
+        self.data.rename(columns=COLUMN_NAME_MAPPING, inplace=True)
 
-    @staticmethod
-    def _is_metadata_row(row: Iterable) -> bool:
-        return not any(x in ["Rec#", "Cyc#", "Step"] for x in [y.value for y in row])
+    def get_column_info(self) -> Dict:
+        """Gathers some metadata for each column.
 
-    def get_metadata(self) -> (Dict, Dict):
-        sheet = self.workbook.sheet_by_index(0)
-        if sheet.ncols < 1 or sheet.nrows < 2:
+        In particular, it gathers if it is a numeric column and if it has data.
+
+        Returns:
+            A nested dictionary with the above information for each column, proved as
+            keys 'is_numeric' and 'has_data.
+        """ ""
+        return {
+            k: {
+                "is_numeric": is_numeric_dtype(self.data[k].dtype),
+                "has_data": True,
+            }
+            for k in self.data.columns
+        }
+
+    def _get_file_header(self) -> Dict[str, Any]:
+        """Extracts the header from the Maccor file.
+
+        The header is spread across columns, so these need to be scan for the key/value
+        pairs to extract.
+
+        Returns:
+            A dictionary with the header information.
+        """
+        if self.sheet.ncols < 1 or self.sheet.nrows < 2:
             raise EmptyFileError()
 
-        metadata = {}
-
-        metadata_end = 0
-        for row in sheet.get_rows():
-            metadata_end += 1
-            if self._is_metadata_row(row):
-                for key, value in zip(row[::2], row[1::2]):  # Pairwise iteration
-                    if not key:
-                        continue
-                    key = key.value.replace("''", "'").strip().rstrip(":")
-                    metadata[key] = self._get_metadata_value(key, value)
-            else:
+        header: Dict[str, Any] = {}
+        for i, row in enumerate(self.sheet.get_rows()):
+            if i == self.skip_rows:
                 break
 
-        metadata["Dataset_Name"] = os.path.basename(self.file_path)
-        metadata["data_start"] = metadata_end
-        metadata["misc_file_data"] = {"excel format metadata": (dict(metadata), None)}
-        metadata["Machine Type"] = "Maccor"
-        metadata["dataset_size"] = os.path.getsize(self.file_path)
-        aggregates, column_info = self._identify_columns(metadata_end - 1)
-        metadata.update(aggregates)
+            current = 0
+            while current < len(row):
+                key, value, current = self._get_metadata_value(current, row)
+                if key == "":
+                    continue
+                header[key] = value
 
-        return metadata, column_info
+        return header
+
+    def get_metadata(self) -> Dict[str, Any]:
+        """Puts together the metadata for the file
+
+        Raises:
+            EmptyFileError: If the file is found to be empty.
+
+        Returns:
+            Dict[str, Any]: A dictionary with the metadata
+        """
+        if self.sheet.ncols < 1 or self.sheet.nrows < 2:
+            raise EmptyFileError()
+
+        metadata: Dict[str, Any] = {
+            "Dataset_Name": self.file_path.stem,
+            "dataset_size": self.file_path.stat().st_size,
+            "num_rows": len(self.data),
+            "data_start": self.skip_rows,
+            "first_sample_no": self.skip_rows + 1,
+            "file_metadata": self._get_file_header(),
+            "Machine Type": "Maccor",
+            "warnings": [],
+        }
+
+        if not self.MANDATORY_COLUMNS.issubset(self.data.columns):
+            metadata["warnings"].append(
+                "Not all mandatory columns are present in the raw datafile"
+            )
+
+        return metadata
 
     def get_data_generator_for_columns(
-        self, columns: List, first_data_row: int, col_mapping: Dict = None
-    ) -> Generator[Dict, None, None]:
-        """
-        Creates a generator for accessing all data in a maccor file row by row for the desired
-         columns. Can rename columns if a custom mapping is passed.
-        """
-        sheet = self.workbook.sheet_by_index(0)
-        col_names = sheet.row_slice(first_data_row - 1)
-        columns_of_interest = [
-            i for i in range(sheet.ncols) if col_names[i].value in columns
-        ]
-        if col_mapping is not None:
-            col_names = [col_mapping[x] if x in col_mapping else x for x in col_names]
+        self, columns: List, first_data_row: int, col_mapping: Optional[Dict] = None
+    ) -> Union[Generator[Dict, None, None], NDArray]:
+        """Provides the data filtered by the requested columns and a column mapping.
 
-        for sheet_id in range(self.workbook.nsheets):
-            logging.info("Loading sheet for data parsing %d", sheet_id)
-            sheet = self.workbook.sheet_by_index(sheet_id)
-            for i in range(first_data_row, sheet.nrows):
-                row = sheet.row(i)
-                yield {
-                    col_names[col].value: (
-                        row[col].value
-                        if self.rec_col != col
-                        else self._sanitise_rec_val(row[col].value)
-                    )
-                    for col in columns_of_interest
-                }
-            logging.info("Unloading sheet %d", sheet_id)
-            self.workbook.unload_sheet(sheet_id)
+        Args:
+            columns (List): Columns of data to provide
+            first_data_row (int): First row of data to parse (NOT USED)
+            col_mapping (Optional[Dict], optional): Dictionary to map the required
+            column names to the Maccor file column names. Defaults to None.
+
+        Raises:
+            KeyError if the required columns, after applying the mapping, do not exist
+                in the data.
+
+        Returns:
+            Union[Generator[Dict, None, None], NDArray]: [description]
+        """
+        cols = (
+            [col_mapping.get(c, c) for c in columns]
+            if col_mapping is not None
+            else columns
+        )
+
+        return self.data[cols].values
 
     @staticmethod
     def _sanitise_rec_val(value) -> float:
@@ -188,3 +238,30 @@ class MaccorXLSParser(ParsingEngineBase):
             return float(value.replace(",", ""))
         else:
             return value
+
+
+def clean_value(value: str) -> str:
+    """Cleans up the string and trims special characters.
+
+    Args:
+        value (str): The string to clean
+
+    Returns:
+        str: The string cleaned.
+    """
+    return value.replace("''", "'").strip().rstrip("\0").strip()
+
+
+def is_metadata_row(row: Iterable, withnesses: Iterable) -> bool:
+    """Checks if a row is a metadata row.
+
+    Args:
+        row (Iterable): Iterable with the row data to check
+        withnesses (Iterable): Iterable with the elements to check that would identify
+            this as NOT a metadata row.
+
+    Returns:
+        bool: True if it is identified as a metadata row
+    """
+    row_values = [y.value if hasattr(y, "value") else y for y in row]
+    return not any(x in withnesses for x in row_values)
