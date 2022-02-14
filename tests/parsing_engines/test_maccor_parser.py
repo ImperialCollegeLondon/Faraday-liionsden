d5527e3e4148159aad299d4a4700cbd8e8cc2423
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-02-14 06:10:37 +0000

    Update MaccorParsingEngine tests

--- tests/parsing_engines/test_maccor_parser.py
+++ tests/parsing_engines/test_maccor_parser.py
@@ -2,27 +2,125 @@ from pathlib import Path
 from types import SimpleNamespace
 from typing import Text
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
 
 
 class TestMaccorXLSParser(TestCase):
+    @patch("parsing_engines.MaccorParsingEngine._drop_unnamed_columns")
+    @patch("parsing_engines.MaccorParsingEngine._standardise_columns")
+    @patch("parsing_engines.MaccorParsingEngine._create_rec_no")
+    @patch("parsing_engines.maccor_engine.get_header_size")
+    @patch("parsing_engines.maccor_engine.load_maccor_data")
+    @patch("parsing_engines.maccor_engine.get_file_header")
+    @patch("parsing_engines.maccor_engine.factory_xlsx")
+    @patch("parsing_engines.maccor_engine.factory_xls")
+    def test_factory(
+        self,
+        mock_xls,
+        mock_xlsx,
+        mock_head,
+        mock_data,
+        mock_size,
+        mock_create,
+        mock_standard,
+        mock_drop,
+    ):
+        from parsing_engines import MaccorParsingEngine as MP
+        from parsing_engines.battery_exceptions import (
+            UnsupportedFileTypeError,
+            EmptyFileError,
+        )
+        import pandas as pd
+
+        sheet = SimpleNamespace(ncols=4, nrows=2)
+        datemode = 1
+        skip_rows = 4
+        mock_xls.return_value = sheet, datemode
+        mock_xlsx.return_value = sheet, datemode
+        mock_data.return_value = pd.DataFrame()
+        mock_size.return_value = skip_rows
+        mock_head.return_value = {"answer": 42}
+
+        # XLS file
+        file_path = Path("maccor_example.xls")
+
+        parser = MP.factory(file_path=file_path)
+        mock_xls.assert_called_once()
+        mock_xlsx.assert_not_called()
+        mock_drop.assert_called_once()
+        mock_standard.assert_called_once()
+        mock_create.assert_called_once()
+        mock_size.assert_called_once_with(sheet, MP.mandatory_columns)
+        mock_data.assert_called_once_with(file_path, skip_rows)
+        mock_head.assert_called_once_with(sheet, skip_rows, datemode)
+        self.assertEqual(len(parser.data), 0)
+        self.assertEqual(parser.name, "maccor")
+        self.assertEqual(parser.skip_rows, skip_rows)
+        self.assertEqual(parser.file_path, file_path)
+        self.assertEqual(parser.file_metadata, {"answer": 42})
+
+        # XLSX file
+        file_path = Path("maccor_example.xlsx")
+        mock_xls.reset_mock()
+        mock_xlsx.reset_mock()
+
+        MP.factory(file_path=file_path)
+        mock_xlsx.assert_called_once()
+        mock_xls.assert_not_called()
+
+        # Empty file
+        sheet = SimpleNamespace(ncols=0, nrows=1)
+        mock_xlsx.return_value = sheet, datemode
+        self.assertRaises(EmptyFileError, MP.factory, file_path)
+
+        # Unsupported file
+        file_path = Path("maccor_example.csv")
+        self.assertRaises(UnsupportedFileTypeError, MP.factory, file_path)
+
+
+class TestMaccorFunctions(TestCase):
     file_path = Path(__file__).parent / "maccor_example.xls"
 
-    def setUp(self) -> None:
-        self.parser = SimpleNamespace(file_path=self.file_path)
-        self.parser.MANDATORY_COLUMNS = {"Cyc#", "Step"}
+    @patch("xlrd.open_workbook")
+    def test_factory_xls(self, mock_open):
+        from parsing_engines.maccor_engine import factory_xls
+
+        sheet = "a sheet"
+        datemode = 1
+        book = SimpleNamespace(
+            sheet_by_index=MagicMock(return_value=sheet), datemode=datemode
+        )
+        mock_open.return_value = book
+
+        sheet, datemode = factory_xls(self.file_path)
+        mock_open.assert_called_with(self.file_path, on_demand=True)
+        self.assertEqual(sheet, sheet)
+        self.assertEqual(datemode, datemode)
+
+    @patch("openpyxl.load_workbook")
+    def test_factory_xlsx(self, mock_open):
+        from parsing_engines.maccor_engine import factory_xlsx
+
+        sheet = "a sheet"
+        book = SimpleNamespace(active=sheet)
+        mock_open.return_value = book
+
+        sheet, datemode = factory_xlsx(self.file_path)
+        mock_open.assert_called_with(self.file_path, read_only=True)
+        self.assertEqual(sheet, sheet)
+        self.assertEqual(datemode, None)
 
     def test_get_header_size(self):
         import xlrd
 
-        from parsing_engines.maccor_engine import get_header_size
+        from parsing_engines.maccor_engine import get_header_size, MaccorParsingEngine
 
         workbook = xlrd.open_workbook(self.file_path, on_demand=True)
-        self.parser.sheet = workbook.sheet_by_index(0)
-        self.parser.datemode = workbook.datemode
-        actual = get_header_size(self.parser.sheet, self.parser.MANDATORY_COLUMNS)
+        sheet = workbook.sheet_by_index(0)
+        actual = get_header_size(sheet, MaccorParsingEngine.mandatory_columns)
 
         expected = 0
-        for i, row in enumerate(self.parser.sheet.get_rows()):
+        for i, row in enumerate(sheet.get_rows()):
             if any(
                 [isinstance(cell.value, Text) and "Cyc#" in cell.value for cell in row]
             ):
@@ -33,130 +131,37 @@ class TestMaccorXLSParser(TestCase):
     def test_load_data(self):
         import xlrd
 
-        from parsing_engines.maccor_engine import get_header_size, load_maccor_data
-
-        workbook = xlrd.open_workbook(self.file_path, on_demand=True)
-        self.parser.sheet = workbook.sheet_by_index(0)
-        self.parser.datemode = workbook.datemode
-        self.parser.skip_rows = get_header_size(
-            self.parser.sheet, self.parser.MANDATORY_COLUMNS
+        from parsing_engines.maccor_engine import (
+            get_header_size,
+            MaccorParsingEngine,
+            load_maccor_data,
         )
 
-        actual = load_maccor_data(self.parser.file_path, self.parser.skip_rows)
+        workbook = xlrd.open_workbook(self.file_path, on_demand=True)
+        sheet = workbook.sheet_by_index(0)
+        skip_rows = get_header_size(sheet, MaccorParsingEngine.mandatory_columns)
+
+        actual = load_maccor_data(self.file_path, skip_rows)
         self.assertGreater(len(actual.columns), 1)
         self.assertGreater(len(actual), 30)
 
-    def test_create_rec_no(self):
-        import xlrd
-
-        from parsing_engines import MaccorParsingEngine as MP
-        from parsing_engines.maccor_engine import get_header_size, load_maccor_data
-
-        workbook = xlrd.open_workbook(self.file_path, on_demand=True)
-        self.parser.sheet = workbook.sheet_by_index(0)
-        self.parser.datemode = workbook.datemode
-        self.parser.skip_rows = get_header_size(
-            self.parser.sheet, self.parser.MANDATORY_COLUMNS
-        )
-        self.parser.data = load_maccor_data(
-            self.parser.file_path, self.parser.skip_rows
-        )
-
-        self.assertNotIn("Rec#", self.parser.data.columns)
-        MP._create_rec_no(self.parser)
-        self.assertIn("Rec#", self.parser.data.columns)
-
-    def test_drop_unnamed_columns(self):
-        import xlrd
-
-        from parsing_engines import MaccorParsingEngine as MP
-        from parsing_engines.maccor_engine import get_header_size, load_maccor_data
-
-        workbook = xlrd.open_workbook(self.file_path, on_demand=True)
-        self.parser.sheet = workbook.sheet_by_index(0)
-        self.parser.datemode = workbook.datemode
-        self.parser.skip_rows = get_header_size(
-            self.parser.sheet, self.parser.MANDATORY_COLUMNS
-        )
-        self.parser.data = load_maccor_data(
-            self.parser.file_path, self.parser.skip_rows
-        )
-
-        if sum(self.parser.data.columns.str.contains("^Unnamed")) > 0:
-            MP._drop_unnamed_columns(self.parser)
-        self.assertEqual(sum(self.parser.data.columns.str.contains("^Unnamed")), 0)
-
-    def test_standardise_columns(self):
-        import xlrd
-
-        from parsing_engines import MaccorParsingEngine as MP
-        from parsing_engines.maccor_engine import get_header_size, load_maccor_data
-        from parsing_engines.mappings import COLUMN_NAME_MAPPING
-
-        workbook = xlrd.open_workbook(self.file_path, on_demand=True)
-        self.parser.sheet = workbook.sheet_by_index(0)
-        self.parser.datemode = workbook.datemode
-        self.parser.skip_rows = get_header_size(
-            self.parser.sheet, self.parser.MANDATORY_COLUMNS
-        )
-        self.parser.data = load_maccor_data(
-            self.parser.file_path, self.parser.skip_rows
-        )
-        MP._drop_unnamed_columns(self.parser)
-
-        def all_cols(data):
-            return sum(
-                [COLUMN_NAME_MAPPING.get(c, c) == c for c in data.columns]
-            ) == len(data.columns)
-
-        if not all_cols(self.parser.data):
-            MP._standardise_columns(self.parser)
-        self.assertTrue(all_cols(self.parser.data))
-
-    def test_get_column_info(self):
-        from parsing_engines import MaccorParsingEngine as MP
-
-        parser = MP.factory(self.file_path)
-        cols = parser.get_column_info()
-        for c in parser.data.columns:
-            self.assertEqual(list(cols[c].keys()), ["is_numeric", "has_data"])
-
     def test_get_file_header(self):
-        from parsing_engines import MaccorParsingEngine as MP
+        import xlrd
 
-        parser = MP.factory(self.file_path)
-        header = parser._get_file_header()
+        from parsing_engines.maccor_engine import (
+            get_header_size,
+            MaccorParsingEngine,
+            get_file_header,
+        )
+
+        workbook = xlrd.open_workbook(self.file_path, on_demand=True)
+        sheet = workbook.sheet_by_index(0)
+        datemode = workbook.datemode
+        skip_rows = get_header_size(sheet, MaccorParsingEngine.mandatory_columns)
+
+        header = get_file_header(sheet, skip_rows, datemode)
         self.assertGreater(len(header), 2)
 
-    def test_get_metadata(self):
-        from parsing_engines import MaccorParsingEngine as MP
-
-        parser = MP.factory(self.file_path)
-        meta = parser.get_metadata()
-        cols = parser.get_column_info()
-
-        self.assertEqual(cols, parser.get_column_info())
-        self.assertEqual(meta["dataset_name"], self.file_path.stem)
-        self.assertEqual(meta["dataset_size"], self.file_path.stat().st_size)
-        self.assertEqual(meta["num_rows"], len(parser.data))
-        self.assertEqual(meta["data_start"], parser.skip_rows)
-        self.assertEqual(meta["first_sample_no"], parser.skip_rows + 1)
-        self.assertEqual(meta["file_metadata"], parser._get_file_header())
-        self.assertEqual(len(meta["warnings"]), 0)
-
-    def test_get_data_generator_for_columns(self):
-        from parsing_engines import MaccorParsingEngine as MP
-
-        parser = MP.factory(self.file_path)
-
-        ncols = 5
-        cols = parser.data.columns[:ncols]
-        actual = list(parser.get_data_generator_for_columns(cols))
-        expected = [list(row) for row in parser.data[cols].values]
-        self.assertEqual(actual, expected)
-
-
-class TestMaccorFunctions(TestCase):
     def test_clean_value(self):
         from parsing_engines.maccor_engine import clean_value
 
