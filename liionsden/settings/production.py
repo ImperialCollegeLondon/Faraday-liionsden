c41e9dafa9e91af7cd9ef993ccc6cab12fddefac
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-11-07 15:36:02 +0000

    Update production.py

--- liionsden/settings/production.py
+++ liionsden/settings/production.py
@@ -13,14 +13,3 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_HSTS_SECONDS = 15552000
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-
-if os.environ.get("WEBSITE_HOSTNAME") == "liionsden.azurewebsites.net":
-    ALLOWED_HOSTS = ["liionsden.azurewebsites.net"]
-    DATABASES["default"]["HOST"] = os.environ["AZURE_POSTGRESQL_POSTGRESQL_40DBE_HOST"]
-    DATABASES["default"]["USER"] = os.environ["AZURE_POSTGRESQL_POSTGRESQL_40DBE_USER"]
-    DATABASES["default"]["PASSWORD"] = os.environ[
-        "AZURE_POSTGRESQL_POSTGRESQL_40DBE_PASSWORD"
-    ]
-    DATABASES["default"]["NAME"] = os.environ["AZURE_POSTGRESQL_POSTGRESQL_40DBE_NAME"]
-    DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
