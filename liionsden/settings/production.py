4aef75d41e61b0d858b8c32f5d7aae705570e1a6
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-11-07 15:35:02 +0000

    update admins

--- liionsden/settings/production.py
+++ liionsden/settings/production.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ["liionsden.rcs.ic.ac.uk"]
 SECRET_KEY = os.environ["SECRET_KEY"]
 EMAIL_HOST = "smarthost.cc.ic.ac.uk"
 SERVER_EMAIL = "noreply@imperial.ac.uk"
-ADMINS = [("Diego Alonso Álvarez", "d.alonso-alvarez@imperial.ac.uk")]
+ADMINS = [("Daniel Davies", "d.w.davies@imperial.ac.uk")]
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
