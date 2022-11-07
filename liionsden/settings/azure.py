abde35dc2f5e0c2658545bd417632e64da80ac17
Daniel Davies <d.w.davies@ucl.ac.uk>
2022-11-07 15:40:50 +0000

    add azure settings

---
+++ liionsden/settings/azure.py
@@ -0,0 +1,70 @@
+import logging
+import os
+import sys
+
+from .production import *  # noqa: F401, F403
+
+# configure sending emails from Imperial mail servers
+EMAIL_HOST = "smtp.cc.ic.ac.uk"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.environ["EMAIL_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_PASSWORD"]
+SERVER_EMAIL = "rcs-noreply@imperial.ac.uk"
+DEFAULT_FROM_EMAIL = "rcs-noreply@imperial.ac.uk"
+
+# Azure postgres requires connections to be encrypted
+DATABASES["default"]["OPTIONS"].update(dict(sslmode="require"))  # noqa: F405
+
+# Below configuration splits console logging into stdout and stderr (for warnings or
+# worse). This helps the Azure AppServiceConsoleLogs mechanism to distinguish between
+# error and information messages in the log analytics workspace.
+
+
+class InfoFilter(logging.Filter):
+    def filter(self, rec):
+        return rec.levelno in (logging.DEBUG, logging.INFO)
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": "{levelname} {asctime} {message}", "style": "{"},
+    },
+    "filters": {
+        "info_filter": {
+            "()": InfoFilter,
+        },
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "handlers": {
+        "console_stdout": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+            "stream": sys.stdout,
+            "level": "DEBUG",
+            "filters": ["info_filter"],
+        },
+        "console_stderr": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+            "stream": sys.stderr,
+            "level": "WARNING",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "root": {"handlers": ["console_stdout", "console_stderr"]},
+    "loggers": {
+        "django": {
+            "handlers": ["mail_admins"],
+            "level": "INFO",
+        },
+    },
+}
