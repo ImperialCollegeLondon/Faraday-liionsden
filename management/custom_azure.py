b04b10111b6ab9470136daaa6434fc6da48b96d1
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-11-10 08:47:58 +0000

    Remove no longer used blob functions

--- management/custom_azure.py
+++ management/custom_azure.py
@@ -1,10 +1,8 @@
-import os
 from datetime import datetime, timedelta
 from logging import getLogger
 
-from azure.core.exceptions import ResourceNotFoundError
 from azure.core.utils import parse_connection_string
-from azure.storage.blob import BlobServiceClient, generate_blob_sas
+from azure.storage.blob import generate_blob_sas
 from django.conf import settings
 
 logger = getLogger()
@@ -44,57 +42,3 @@ def generate_sas_token(blob_name, permission="r"):
     )
 
     return sas_token
-
-
-def download_blob(local_path="./tmp", blob_name="", sas_token=""):
-    """
-    TODO: This is not used. Remove and replace with a function to get the correct
-    blob URL, which are used by the views. Correct URL based on whether in local
-    dev or production.
-    """
-    blob_endpoint = get_blob_endpoint()
-    try:
-        os.makedirs(os.path.join(local_path, "uploaded_files"), exist_ok=True)
-        blob_service_client = BlobServiceClient(blob_endpoint, sas_token)
-        blob_client = blob_service_client.get_container_client(
-            container=settings.AZURE_CONTAINER
-        )
-        download_path = os.path.join(local_path, blob_name)
-        with open(download_path, "wb") as f:
-            f.write(blob_client.download_blob(blob_name).readall())
-
-        return download_path
-
-    except ResourceNotFoundError:
-        logger.error(
-            f"Could not find blob {blob_name} in container {settings.AZURE_CONTAINER}"
-        )
-        return None
-
-
-def delete_blobs(blob_names, container):
-    """
-    Deletes the given blobs from the Azure storage account.
-    """
-    blob_endpoint = get_blob_endpoint()
-    account_key = parse_connection_string(settings.AZURE_CONNECTION_STRING)[
-        "accountkey"
-    ]
-    blob_service_client = BlobServiceClient(blob_endpoint, account_key)
-    blob_client = blob_service_client.get_container_client(container=container)
-    for blob_name in blob_names:
-        blob_client.delete_blob(blob_name)
-    return blob_names
-
-
-def list_blobs(container):
-    """
-    Lists all the blobs in the Azure storage account.
-    """
-    blob_endpoint = get_blob_endpoint()
-    account_key = parse_connection_string(settings.AZURE_CONNECTION_STRING)[
-        "accountkey"
-    ]
-    blob_service_client = BlobServiceClient(blob_endpoint, account_key)
-    blob_client = blob_service_client.get_container_client(container=container)
-    return blob_client.list_blobs()
