05fe9ad9387151854309e6932cee0e90328dd379
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-11-14 17:47:11 +0000

    primary_blob_connection_string

--- Main.tf
+++ Main.tf
@@ -188,7 +188,7 @@ resource "azurerm_linux_web_app" "app" {
     EMAIL_PASSWORD                  = var.email_password
     ADMIN_PASSWORD                  = var.admin_password
     AZURE_STORAGE_CONTAINER         = azurerm_storage_container.media.name
-    AZURE_STORAGE_CONNECTION_STRING = azurerm_storage_account.media.primary_connection_string
+    AZURE_STORAGE_CONNECTION_STRING = azurerm_storage_account.media.primary_blob_connection_string
     AZURE_ACCOUNT_NAME              = azurerm_storage_account.media.name
   }
   logs {
